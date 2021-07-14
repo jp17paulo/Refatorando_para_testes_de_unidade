9133772d9cca7bbd11b8fa002d68fbb6f5876117
João Paulo <jp17paulo@terra.com.br>
2021-07-14 17:50:53 -0300

    Correção na classe Order

--- Store.Tests/Entities/OrderTests.cs
+++ Store.Tests/Entities/OrderTests.cs
@@ -17,26 +17,32 @@ namespace Store.Tests.Domain
         [TestCategory("Domain")]
         public void Dado_um_novo_pedido_valido_ele_deve_gerar_um_numero_com_8_caracteres()
         {
-            // var order = new Order(_customer, 0, null);
-            // Assert.AreEqual(8, order.Number.Length);
+            /******Testes iniciais*************/
+            //  var customer = new Customer("André Baltieri", "andre@balta.io");
+            //  var order = new Order(customer, 0, null);
+            //  Assert.AreEqual(8, order.Number.Length);
+
+            /******Após refatoração************/
+            var order = new Order(_customer, 0, null);
+            Assert.AreEqual(8, order.Number.Length);
         }
 
         [TestMethod]
         [TestCategory("Domain")]
         public void Dado_um_novo_pedido_seu_status_deve_ser_aguardando_pagamento()
         {
-            // var order = new Order(_customer, 0, null);
-            // Assert.AreEqual(order.Status, EOrderStatus.WaitingPayment);
+            var order = new Order(_customer, 0, null);
+            Assert.AreEqual(order.Status, EOrderStatus.WaitingPayment);
         }
 
         [TestMethod]
         [TestCategory("Domain")]
         public void Dado_um_pagamento_do_pedido_seu_status_deve_ser_aguardando_entrega()
         {
-            // var order = new Order(_customer, 0, null);
-            // order.AddItem(_product, 1); // Total deve ser 10
-            // order.Pay(10);
-            // Assert.AreEqual(order.Status, EOrderStatus.WaitingDelivery);
+            var order = new Order(_customer, 0, null);
+            order.AddItem(_product, 1); // Total deve ser 10
+            order.Pay(10);
+            Assert.AreEqual(order.Status, EOrderStatus.WaitingDelivery);
         }
 
         [TestMethod]
