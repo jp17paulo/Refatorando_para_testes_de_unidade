8f51d0e189ed506b28e95872ffb90f7a84e3322d
jp17paulo <jp17paulo@terra.com.br>
2021-08-07 11:24:55 -0300

    Criação da lógica para o método Dado_um_novo_item_sem_produto_o_mesmo_nao_deve_ser_adicionado() da classe OrderTests.cs

--- Store.Tests/Entities/OrderTests.cs
+++ Store.Tests/Entities/OrderTests.cs
@@ -58,9 +58,9 @@ namespace Store.Tests.Domain
         [TestCategory("Domain")]
         public void Dado_um_novo_item_sem_produto_o_mesmo_nao_deve_ser_adicionado()
         {
-            // var order = new Order(_customer, 0, null);
-            // order.AddItem(null, 10);
-            // Assert.AreEqual(order.Items.Count, 0);
+            var order = new Order(_customer, 0, null);
+            order.AddItem(null, 10);
+            Assert.AreEqual(order.Items.Count, 0);
         }
 
         [TestMethod]
